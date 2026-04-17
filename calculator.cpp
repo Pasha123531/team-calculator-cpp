7a9a2dbae0c8e1c26c4f0cc371ef991a0244a323
HusinskyiPavlo <pashok190408@gmail.com>
2026-04-17 20:01:38 +0300

    Implement basic calculator functions and main

---
+++ calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "calculator.h"
+
+
+double Calculator::Add(double a, double b) {
+    return a + b;
+}
+
+double Calculator::Subtract(double a, double b) {
+    return a - b;
+}
+
+
+int main() {
+    Calculator calc;
+    std::cout << "Командний калькулятор запущено!" << std::endl;
+  
+    return 0;
+}
