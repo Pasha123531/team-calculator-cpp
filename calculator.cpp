fa63c056da59d376dd3ee542f781a297e1fe5522
Владік Путінца <vladputintsa@gmail.com>
2026-04-18 13:16:44 +0300

    Vlad: Implement Power and Percentage methods with tests

--- calculator.cpp
+++ calculator.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <cmath>
 #include "calculator.h"
 
-
+//Sum of two numbers
 double Calculator::Add(double a, double b) {
     return a + b;
 }
 
+//Difference of two numbers
 double Calculator::Subtract(double a, double b) {
     return a - b;
 }
 
+//Power of a number
+double Calculator::Power(double base, double exponent) {
+    return std::pow(base, exponent);
+}
 
-int main() {
-    Calculator calc;
-    std::cout << "Командний калькулятор запущено!" << std::endl;
-  
-    return 0;
-    
+//Percentage of a number
+double Calculator::Percentage(double total, double percent) {
+    return (total * percent) / 100.0;
 }
