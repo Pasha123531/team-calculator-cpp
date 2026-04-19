b28be4c4d7c6d5f5f196bd3350d5de9ff51c14c3
Demyan <musiv31101979@gmail.com>
2026-04-19 19:06:55 +0300

    Demian: Implement Multiply and Divide

--- calculator.cpp
+++ calculator.cpp
@@ -11,6 +11,14 @@ double Calculator::Subtract(double a, double b) {
 }
 
 
+double Calculator::Multiply(double a, double b) {
+    return a * b;
+}
+double Calculator::Divide(double a, double b) {
+    if (b == 0) return 0; // Базова перевірка
+    return a / b;
+}
+
 int main() {
     Calculator calc;
     std::cout << "Командний калькулятор запущено!" << std::endl;
